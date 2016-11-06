4a65a9f08087a8f458444f8067272e534459daaa
Li Richard <lirichard@Richard-RMBP-D1R2.local>
2016-11-06 02:24:10 -0800

    update version to 20161106
    fixed typo issue for packages readEDID

--- Formula/readedid.rb
+++ Formula/readedid.rb
@@ -2,7 +2,7 @@ require "formula"
 class Readedid < Formula
     homepage "https://github.com/othercat/readEDID"
     stable do
-    url "https://github.com/othercat/readEDID", :using => :git, :tag => "20161105"
+    url "https://github.com/othercat/readEDID", :using => :git, :tag => "20161106"
 end
 devel do
     url 'https://github.com/othercat/readEDID.git', :branch => 'master'
@@ -13,8 +13,8 @@ depends_on :macos => :snow_leopard
 def install
     system "cmake", "-G","Xcode", "./readEDID/"
     xcodebuild "-target readedid SYMROOT=build CONFIGURATION_BUILD_DIR=bin"
-    system "mkdir","/usr/local/Cellar/readedid/20161105/bin/"
-    system "cp","bin/Debug/readedid","/usr/local/Cellar/readedid/20161105/bin/"
+    system "mkdir","/usr/local/Cellar/readedid/20161106/bin/"
+    system "cp","bin/Debug/readedid","/usr/local/Cellar/readedid/20161106/bin/"
     #system "cp","doc/readedid.1","/usr/local/Cellar/foo/0.1/share/man/man1/"
 end
 test do
